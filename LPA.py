83b284ff01d193f4f88cbae3a7f01dd33bb6c0c0
albret <huangawh@gmail.com>
2017-11-20 03:01:47 -0500

    Changed terminate condition. Add options for verbose and output or show each iteration to file.

--- LPA.py
+++ LPA.py
@@ -6,7 +6,7 @@ class ImageProcessing:
 	def __init__(self, filename):
 		self.img = Image.open(filename)
 		self.bytes = self.img.tobytes()
-		self.bytesFresh = False
+		self.bytesFresh = True
 		self.iwidth, self.iheight = self.img.size
 		self.pixels = []
 		self.getRGB()
@@ -23,6 +23,64 @@ class ImageProcessing:
 				xcur.append(temp)
 			self.pixels.append(xcur)
 
+	def kmeans(self, k, seedGen=None, show=False, toFile=False, verbose=False):
+		if seedGen is None:
+			cluster = self.randomSeeds(k)
+		else:
+			cluster = seedGen(k)
+		assignments = [[-1 for x in range(self.iwidth)] for y in range(self.iheight)]
+		iterCount = 0
+
+		while True:
+			iterCount += 1
+			reassignment = 0
+			sse = [0 for x in range(k)]
+			newCluster = [[0, 0, 0, 0] for x in range(k)]
+			for x in range(self.iheight):
+				for y in range(self.iwidth):
+					minSim = 1000
+					index = -1
+					for z in range(k):
+						minSim, index = min((minSim, index), (self.imageSimilarity(cluster[z], self.pixels[x][y]), z))
+					if assignments[x][y] != index:
+						assignments[x][y] = index
+						reassignment += 1
+					sse[index] += minSim
+					newCluster[index] = [
+						newCluster[index][z] + self.pixels[x][y][z] if z < 3 else newCluster[index][z] + 1
+						for z in range(4)]
+			newCluster = [
+				[newCluster[x][y] // newCluster[x][3] if newCluster[x][3] != 0 else newCluster[x][y] for y
+				 in range(3)] for x in range(k)]
+
+			if verbose:
+				print('iteration number:', iterCount)
+				print('old cluster:', cluster, sep='\n')
+				print('new cluster:', newCluster, sep='\n')
+				print('number of reassignments:', reassignment)
+				print('cluster SSE', sse, '\n')
+
+			if toFile or show:
+				fn = self.getFilename('kmean', k, iterCount)
+				tempData = [[newCluster[assignments[x][y]] for y in range(self.iwidth)] for x in range(self.iheight)]
+				self.toImage(tempData, show=show, toFile=toFile, filename=fn)
+
+			if self.kmterminate(k, cluster, newCluster):
+				return
+			cluster = newCluster
+
+	def kmterminate(self, k, old, new):
+		for x in range(k):
+			change = 0
+			for y in range(3):
+				temp = abs(old[x][y] - new[x][y])
+				if temp > 1:
+					return False
+				change += temp
+			if change > 3:
+				return False
+		return True
+
 	def imageSimilarity(self, a, b):
 		rmean = (a[0] + b[0]) // 2
 		rdiff = a[0] - b[0]
@@ -31,9 +89,22 @@ class ImageProcessing:
 		return ((((512 + rmean) * rdiff * rdiff) >> 8) + 4 * gdiff * gdiff + (
 			((767 - rmean) * bdiff * bdiff) >> 8)) ** 0.5
 
-	def toImage(self, data=None):
+	def randomSeeds(self, k):
+		output = set()
+		while len(output) != k:
+			output.add((randrange(0, self.iheight), randrange(0, self.iwidth)))
+		output = [self.pixels[x[0]][x[1]] for x in output]
+		return list(output)
+
+	def getFilename(self, method, k, iteration):
+		return 'output/' + method + '_' + str(k) + '_' + str(iteration) + '.png'
+
+	def toImage(self, data=None, show=True, toFile=False, filename=''):
 		tempImg = Image.frombuffer('RGB', (self.iwidth, self.iheight), bytes(self.toByteArr(data)), 'raw', 'RGB', 0, 1)
-		tempImg.show()
+		if show:
+			tempImg.show()
+		if toFile:
+			tempImg.save(filename, 'png')
 
 	def toByteArr(self, data=None):
 		if data is None:
@@ -47,54 +118,14 @@ class ImageProcessing:
 			output = bytearray(output)
 			return output
 
-	def kmeans(self, k, seedGen=None):
-		if seedGen is None:
-			cluster = self.randomSeeds(k)
-		else:
-			cluster = seedGen(k)
-		assignments = [[-1 for x in range(self.iwidth)] for y in range(self.iheight)]
-		reassignment = self.iwidth * self.iheight
-		iterCount = 1
-
-		while reassignment > int(self.iwidth * self.iheight * 0.02):
-			reassignment = 0
-			sse = [0 for x in range(k)]
-			clusterUpdate = [[0, 0, 0, 0] for x in range(k)]
-			for x in range(self.iheight):
-				for y in range(self.iwidth):
-					minSim = 1000
-					index = -1
-					for z in range(k):
-						minSim, index = min((minSim, index), (self.imageSimilarity(cluster[z], self.pixels[x][y]), z))
-					if assignments[x][y] != index:
-						assignments[x][y] = index
-						reassignment += 1
-					sse[index] += minSim
-					clusterUpdate[index] = [
-						clusterUpdate[index][z] + self.pixels[x][y][z] if z < 3 else clusterUpdate[index][z] + 1
-						for z in range(4)]
-			print('iteration number:', iterCount)
-			print('old cluster:', cluster, sep='\n')
-			cluster = [[clusterUpdate[x][y] // clusterUpdate[x][3] for y in range(3)] for x in range(k)]
-			print('new cluster:', cluster, sep='\n')
-			print('number of reassignments:', reassignment)
-			print('cluster SSE', sse)
-			iterCount += 1
-			self.toImage([[cluster[assignments[x][y]] for y in range(self.iwidth)] for x in range(self.iheight)])
-
-	def randomSeeds(self, k):
-		output = set()
-		while len(output) != k:
-			output.add((randrange(0, self.iheight), randrange(0, self.iwidth)))
-		output = [self.pixels[x[0]][x[1]] for x in output]
-		return list(output)
-
 
 if __name__ == '__main__':
-	tester = ImageProcessing('image000.jpg')
-	tester.toImage()
-	tester.kmeans(int(input("Enter k:")))
+	tester = ImageProcessing('imageData/image003.jpg')
+	# tester.toImage()
+	tester.kmeans(int(input("Enter k:")), toFile=True, verbose=True)
 
-# TODO k means
-# TODO visualize
+
+# TODO do i even need to make 3d array and change to int?
+# TODO can kmean be optimized? 
+# TODO cluster only if touching
 # TODO B
